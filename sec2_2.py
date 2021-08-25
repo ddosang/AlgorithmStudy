2f37f10b15cef7bf2990ee00f04a993ed51c4fb1
ddosang <218926@naver.com>
2021-08-25 22:31:55 +0900

    2-2. k번째 수 (#1)

--- sec2_2.py
+++ sec2_2.py
@@ -14,7 +14,7 @@ for i in range(t):
     n, s, e, k = map(int, input().split())
 
     array = input().split()
-    array = list(map(int, array))
+    array = list(map(int, array))  #array에 바로 받을 수 있음. map 함수 앞에 list로 묶기.
 
     arr = array[s-1:e]
     arr.sort()
