14639ad7a480ae3be7ef6a5ef7828d45afddc4a2
ddosang <218926@naver.com>
2021-08-30 00:38:46 +0900

    4-2. 랜선자르기 (#3)

--- sec4/sec4_02.py
+++ sec4/sec4_02.py
@@ -4,6 +4,7 @@ k, n = map(int, input().split())
 lineLengths = []
 totalLength = 0
 
+#1 시간 초과 40점
 for i in range(k):
     length = int(input())
     lineLengths.append(length)
@@ -22,3 +23,21 @@ for i in range(maxLengthMath, 0, -1):
     if count == n:
         print(i)
         break
+
+
+#2. 강의 듣고 짠 것 (이분 검색)
+start = 1
+end = maxLengthMath #1~이론최대길이 까지 이분 검색을 해서
+max = 0
+while start <= end:
+    count = 0
+    mid = (start + end) // 2
+    for i in range(len(lineLengths)):
+        count += lineLengths[i] // mid
+
+    if count >= n:
+        if max < mid: #count>=n을 만족하는 것 중 최대 길이 찾아낸다.
+            max = mid
+        start = mid + 1
+    else:
+        end = mid - 1
