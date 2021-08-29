659879d75422c6fff97512d1f9fb6c9b5d17c21a
ddosang <218926@naver.com>
2021-08-30 02:53:07 +0900

    4-3. 뮤직비디오 (#3)

--- sec4/sec4_03.py
+++ sec4/sec4_03.py
@@ -1,5 +1,4 @@
 
-
 def countDVD(time):
     dvd = 1
     scatterTime = 0 #곡 개수 별로 쪼갠 시간
@@ -11,18 +10,12 @@ def countDVD(time):
         if scatterTime > time: #순차적으로 더하다가 time보다 커지면
             scatterTime -= musicTimes[i] #그 앞 노래까지 넣을 수 있음.
             i -= 1 #다음에는 시간을 넘긴 그 노래부터 넣어야하니까 줄인다.
-            if scatterTime > maxTime:
-                maxTime = scatterTime
-
             dvd += 1
             scatterTime = 0
 
-        if i == n-1: #맨 마지막 dvd 의 시간도 계산
-            if scatterTime > maxTime:
-                maxTime = scatterTime
         i += 1
+    return dvd
 
-    return dvd, maxTime
 
 #입력 받기
 n, m = map(int, input().split())
@@ -34,19 +27,18 @@ for i in range(n):
     totalTime += musicTimes[i]
 
 minTime = totalTime
-avgMusicTime = totalTime // m
+avgMusicTime = totalTime // n
+maxMusicTime = max(musicTimes)
 
 #이분탐색
 start = avgMusicTime
 end = totalTime
 while start <= end:
     mid = (start + end) // 2
-    dvdCount, time = countDVD(mid)
 
-    if dvdCount <= m: #dvd 개수가 적으면
+    if mid >= maxMusicTime and countDVD(mid) <= m: #dvd 개수가 적으면
+        minTime = mid
         end = mid - 1 #시간을 줄여야 함.
-        if minTime > time: #그 중 가장 작은 시간이 필요함.
-            minTime = time
     else: #dvd 개수가 많으면 시간을 늘려야함.
         start = mid + 1
 
