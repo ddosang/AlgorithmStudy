8372e89957f630bc64b96e9bfe497aa53bd36dc6
ddosang <218926@naver.com>
2022-01-25 23:22:53 +0900

    0125 비밀지도 (py)

--- Programmers_level1/비밀지도.py
+++ Programmers_level1/비밀지도.py
@@ -8,6 +8,7 @@ def solution(n, arr1, arr2):
         res = arr1[i] | arr2[i] # or
         res = str(bin(res))[2:] # 0b 제거
         res = res if len(res) == n else ''.join(['0' for _ in range(n-len(res))]) + res # n자리가 아니면 n자리로 만들어줌
+        # res = res if len(res) == n else ('0' * (n-len(res))) + res # 다시 풀었을 때 바뀐 부분.
         res = re.sub('1', '#', res) #정규표현식으로 1은 #
         res = re.sub('0', ' ', res) #0은 공백으로
         answer.append(res)
