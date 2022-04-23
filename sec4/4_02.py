564d31070d56ed83efd7536e001585eae7bbe9d9
ddosang <40856360+ddosang@users.noreply.github.com>
2022-04-23 12:35:07 +0900

    0423 재복습

--- sec4/4_02.py
+++ sec4/4_02.py
@@ -19,3 +19,28 @@ while length > 0:
         possible.append(length)
 
 print(max(possible))
+
+
+k, n = map(int, input().split())
+line_lengths = [int(input()) for _ in range(k)]
+
+start = 1
+end = max(line_lengths)
+max_split_length = 0
+
+while start <= end:
+    mid = (start + end) // 2
+    count = 0
+    for line in line_lengths:
+        count += line // mid
+
+    if count >= n:
+        if max_split_length < mid:
+            max_split_length = mid
+        start = mid + 1
+    else:
+        end = mid - 1
+
+
+print(max_split_length)
+
