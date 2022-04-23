be434734f273215b95e88bdb81347df154246f07
ddosang <40856360+ddosang@users.noreply.github.com>
2022-04-23 13:04:10 +0900

    0423 재복습

--- sec4/4_05.py
+++ sec4/4_05.py
@@ -17,3 +17,20 @@ for i in range(1, n):
         end = meetings[i][1]
 
 print(count)
+
+
+# 재복습
+n = int(input())
+interviews = [list(map(int, input().split())) for _ in range(n)]
+
+interviews.sort(key=lambda x: x[1])
+
+count = 1
+last_interview = 0
+
+for i in range(1, n):
+    if interviews[last_interview][1] <= interviews[i][0]:
+        count += 1
+        last_interview = i
+
+print(count)
