f0434a18d5ba23e2a9c54aebf931fc0529d548e7
ddosang <40856360+ddosang@users.noreply.github.com>
2022-04-26 19:43:59 +0900

    0426 복습

--- sec5/5_03.py
+++ sec5/5_03.py
@@ -23,7 +23,8 @@ for exp in expression:
         if priority[exp] > p:
             op_stack.append(exp)
         else:
-            while op_stack and pop_priority[op_stack[-1]] >= priority[exp]: # 다른데서 놓친 줄 알았는데 priority[exp] 대신 p를 써서 이상하게 나오는 거였음.
+            while op_stack and pop_priority[op_stack[-1]] >= priority[exp]: 
+            # 다른데서 놓친 줄 알았는데 priority[exp] 대신 p를 써서 같은 순위면 스택에서 못빠져나가서 이상하게 나오는 거였음.
                 answer += op_stack.pop()
             op_stack.append(exp)
         p = priority[exp]
