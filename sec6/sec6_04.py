6d8116bef141b8348924e6ee96ff035303cbc7c2
ddosang <40856360+ddosang@users.noreply.github.com>
2022-05-04 16:25:23 +0900

    Create sec6_04.py

---
+++ sec6/sec6_04.py
@@ -0,0 +1,63 @@
+# 혼자 짠 코드..
+# return True 로는 재귀를 탈출할 수가 없다는 것이 문제점.
+
+def DFS(index):
+    if index == n+1:
+        local_sum = 0
+        for i in range(1, n+1):
+            if check[i] == 1:
+                local_sum += nums[i - 1]
+
+        if local_sum == total - local_sum:
+            # print("YES")
+            return True
+
+        # print(local_sum, check)
+
+    else:
+        check[index] = 1
+        DFS(index + 1)
+        check[index] = 0
+        DFS(index + 1)
+
+    return False
+
+n = int(input())
+nums = list(map(int, input().split()))
+total = sum(nums)
+check = [0] * (n + 1)
+
+
+if DFS(1):
+    print("YES") # YES 가 2번 찍힘..
+else:
+    print("NO")
+
+
+
+
+    
+# 강의 코드
+# check 를 쓰지 않고, sum 을 이용해서!!
+import sys
+
+def DFS(index, sum):
+    if index == n: # 0번 index 부터 사용.
+        if sum == total - sum:
+            print("YES")
+            sys.exit(0) # 걍 냅다 종료 시켜버려~
+
+        # print(local_sum, check)
+
+    else:
+        DFS(index + 1, sum + nums[index])
+        DFS(index + 1, sum)
+
+
+n = int(input())
+nums = list(map(int, input().split()))
+total = sum(nums)
+
+DFS(0, 0)
+
+print("NO") # 종료가 안됐다면 없는거니까 NO
