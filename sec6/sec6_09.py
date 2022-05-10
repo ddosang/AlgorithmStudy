ca99c5f8b46add85be3455f6219617eb2f6764bb
ddosang <40856360+ddosang@users.noreply.github.com>
2022-05-10 14:26:33 +0900

    9. 수열 추측하기

---
+++ sec6/sec6_09.py
@@ -0,0 +1,83 @@
+# 개념만 듣고 풀었더니 파스칼 삼각형 식을...
+# n! / r! 으로 새우면.....? 매우 비효율적일 것 같아서 막혀있었다.
+
+import sys
+
+n, f = map(int, input().split())
+
+def DFS(level):
+    if level == n:
+        local_sum = 0
+        for i in range(n):
+            local_sum += b[i] * res[i]
+
+        if local_sum == f:
+            for i in range(n):
+                print(res[i], end=' ')
+            sys.exit(0)
+
+        return
+
+    else:
+        for i in range(1, n+1):
+            if check[i] != 1:
+                check[i] = 1
+                res[level] = i
+                DFS(level + 1)
+                check[i] = 0
+
+
+check = [0] * (n + 1)
+b = [1] * n # 이항계수
+res = [0] * n # 조합
+
+
+# 수업에 나온 대박적 식
+for i in range(1, n - 1):
+    b[i] = b[i - 1] * (n - i) / i
+
+
+DFS(0)
+
+
+
+# 수업 코드
+
+
+import sys
+
+n, f = map(int, input().split())
+
+def DFS(level, sum): # sum 넘기는게 훨씬 깔끄.
+    if sum > f:
+        return
+
+    if level == n 
+        if sum == f:
+            for i in range(n):
+                print(res[i], end=' ')
+            sys.exit(0)
+        return
+
+    else:
+        for i in range(1, n+1):
+            if check[i] != 1:
+                check[i] = 1
+                res[level] = i
+                DFS(level + 1, sum + b[level] * res[level])
+                check[i] = 0
+
+
+check = [0] * (n + 1)
+b = [1] * n # 이항계수
+res = [0] * n # 조합
+
+
+for i in range(1, n - 1):
+    b[i] = b[i - 1] * (n - i) / i
+
+
+DFS(0, 0)
+
+
+
