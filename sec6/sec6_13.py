1c5c5cee166cff25c0aac95a904902d76d2b904c
ddosang <40856360+ddosang@users.noreply.github.com>
2022-05-10 15:51:24 +0900

    13. 경로 탐색

---
+++ sec6/sec6_13.py
@@ -0,0 +1,34 @@
+# C언어 DFS 살짜쿵 보고 만듬. -> 그냥 한가지 경로만 찾는 것.
+# 여러 경로를 찾아야하니까 DFS 에서 ch 를 이용해야한다.
+
+def DFS(start):
+    global cnt
+
+    # 연결 그래프가 아니면 무한루프..
+    if start == n - 1:
+        cnt += 1
+        return
+
+    else:
+        ch[start] = 1
+        for i in range(n):
+            if arr[start][i] == 1 and ch[i] == 0:
+                DFS(i)
+                ch[i] = 0 # 꺾는 것은 DFS 에서 방문한 곳 기준이니까.
+
+
+
+n, m = map(int, input().split())
+
+arr = [[0 for _ in range(n)] for _ in range(n)]
+ch = [0] * n
+
+for i in range(m):
+    start, end = map(int, input().split())
+    arr[start - 1][end - 1] = 1
+
+res = [0] * n
+cnt = 0
+
+DFS(0)
+print(cnt)
