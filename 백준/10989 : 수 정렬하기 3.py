d70f73bbd3dde51afe39560cccfa08e0a456df62
ddosang <40856360+ddosang@users.noreply.github.com>
2022-10-20 15:35:45 +0900

    Update 10989 : 수 정렬하기 3.py

--- 백준/10989 : 수 정렬하기 3.py
+++ 백준/10989 : 수 정렬하기 3.py
@@ -1,21 +1,12 @@
-n = int(input())
-arr = [int(input()) for _ in range(n)]
+import sys
 
-local_min = 10000001
-
-for i in range(n):
-    local_min = 10000001
-    for j in range(i + 1, n):
-        if local_min > arr[j]:
-            local_min = arr[j]
-            local_min_index = j
-
-    if local_min < arr[i]:
-        temp = arr[i]
-        arr[i] = arr[local_min_index]
-        arr[local_min_index] = temp
-
-    print(arr)
+n = int(sys.stdin.readline())
+arr = [0] * 10001
 
 for i in range(n):
-    print(arr[i])
+    index = int(sys.stdin.readline())
+    arr[index] += 1
+
+for i in range(1, 10001):
+    for j in range(arr[i]):
+        print(arr[i])
