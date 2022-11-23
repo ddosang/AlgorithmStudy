ac61427b328cbf840b5a602e5b280de5e4b00342
ddosang <218926@naver.com>
2022-11-23 09:18:19 +0900

    1122 Social Distancing 설명 추가

--- 백준/18877 : Social Distancing.py
+++ 백준/18877 : Social Distancing.py
@@ -36,6 +36,29 @@ def check(d):
     return cow
 
 
+def check2(d):
+    # 첫번째 소 배치
+    idx = 0
+    last = grasses[0][0]
+
+    # 나머지 n - 1 마리의 소에 대해서
+    for _ in range(n - 1):
+        # 잔디 구간이 더 작으면 다음 잔디구간으로 넘어감.
+        while idx < m and grasses[idx][1] < last + d:
+            idx += 1
+        
+        # 위에서 나왔는데 잔디 구간이 남아있지 않아서 나온거면, 
+        # 놓을 수 있는 cow 가 더 적은거니까 더 이상 검사할 필요 X
+        if idx == m:
+            return False
+        
+        # + d 한게 다음 잔디 구간 안에 있으면 + d 아니면 다음 잔디 구간 첫번째로 바꿈.
+        last = grasses[idx][0] if grasses[idx][0] > last + d else last + d
+
+    return True
+
+
+
 n, m = map(int, readl().split())
 grasses = [list(map(int, readl().split())) for _ in range(m)]
 
