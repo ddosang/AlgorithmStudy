54151e74322e03193b6c03ad4e171adcd2c0740d
ddosang <40856360+ddosang@users.noreply.github.com>
2024-04-17 23:03:36 +0900

    240417 복습

--- sec2/2_10.py
+++ sec2/2_10.py
@@ -1,3 +1,22 @@
+# 240417 
+# 뭔가 깔끔하게 풀었는데?
+N = int(input())
+answers = list(map(int, input().split()))
+
+score = 0
+cnt = 0 # 연속된 개수 세기
+for a in answers:
+    if a == 1:
+        cnt += 1
+        score += cnt # 그 개수가 점수
+    else:
+        cnt = 0 # 틀린게 나오면 초기화
+
+print(score)
+
+
+
+#이전에 푼 것.
 n = int(input())
 scores = list(map(int, input().split()))
 
