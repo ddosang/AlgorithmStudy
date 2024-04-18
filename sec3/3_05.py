976cb9495844973700cd57f4bb589d1ee94b0f0e
ddosang <40856360+ddosang@users.noreply.github.com>
2024-04-18 22:56:11 +0900

    240418 복습

--- sec3/3_05.py
+++ sec3/3_05.py
@@ -18,3 +18,28 @@ while rt <= n:
         rt += 1
 
 print(count)
+
+
+# 240418
+# sum 을 이용하지 않으면 while 문이 한번 돌 때 마다, 
+# arr 의 하나의 요소에만 접근하면 됨.
+N, M = map(int, input().split())
+nums = list(map(int, input().split()))
+
+
+cnt = 0
+lt = 0
+rt = 1
+local_sum = nums[lt]
+while lt < rt:
+    if local_sum == M:
+        cnt += 1
+    
+    if local_sum <= M and rt < N:
+        local_sum += nums[rt]
+        rt += 1
+    else:
+        local_sum -= nums[lt]
+        lt += 1
+
+print(cnt)
