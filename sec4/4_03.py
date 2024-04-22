84450c7928a055ae5d047cf178792d6ad3b6d391
ddosang <40856360+ddosang@users.noreply.github.com>
2024-04-22 21:29:54 +0900

    240422 복습

--- sec4/4_03.py
+++ sec4/4_03.py
@@ -1,131 +1,41 @@
-import sys
-
-def countDVD(time):
-    dvd = 1
-    scatterTime = 0
-    maxTime = 0
-    i = 0
-
-    while i < n:
-        # 노래를 넣다가
-        scatterTime += times[i]
-
-        # dvd 시간을 초과하면 맨 마지막거 하나 뺴고
-        # dvd 녹화 최대 시간 경신, dvd 장수 추가, scatterTime 초기화
-        if scatterTime > time:
-            scatterTime -= times[i]
-            i -= 1
-
-            if maxTime < scatterTime:
-                maxTime = scatterTime
-
-            dvd += 1
-            scatterTime = 0
-
-        # 맨 마지막 dvd 는 time 보다 길게 녹화되지 않으니가
-        # 따로 검사해줘야함.
-        if i == n-1:
-            if maxTime < scatterTime:
-                maxTime = scatterTime
-
-        i += 1
-
-    return dvd, maxTime
-
-
-
-
-# sys.stdin = open("input.txt", "rt")
-
-# 시간이 다 다른 n개의 곡을 m개의 씨디(같은 시간)에 쪼개 넣기.
-n, m = map(int, input().split())
-times = list(map(int, input().split()))
-possible = []
-
-
-# 평균부터 시작.
-start = sum(times) // m
-end = sum(times)
-
-minTime = end
-
-while start <= end:
-    mid = (start + end) // 2
-    # 이분 탐색으로 시간을 넣어서 녹화를 해봄.
-    # 해당 시간 안에 dvd 개수가 더 많이 녹화됨 -> 시간을 늘려야 됨.
-    # dvd 개수가 더 작거나 같게 녹화됨 -> 시간을 줄여야 됨. (제일 작은 시간을 찾는게 목적이니 개수가 같은 것도 포함)
-    # start 와 end 가 교차되면 끝.
-    dvd, time = countDVD(mid)
-
-    if mid >= max(times) and dvd <= m:
-        minTime = mid
-        end = mid - 1
-        if minTime > time:
-            minTime = time
-    else:
-        start = mid + 1
-
-print(minTime)
-
-
-
-
-# 0423
-# k 개를 잘라서 n개로 만들 것임.
-
-def countDVDandTime(time):
-    dvd = 0
+# 240422
+# 과거의 나는 time check_count 에서 max_time 을 왜 저장하려고 한지 모르겠음.
+def check_count(mid):
+    local_cd = 0
+    cnt = 0
     max_time = 0
-    local_time = 0
-    i = 0
+    for i in range(len(arr)):
+        local_cd -= arr[i]
+        if local_cd < 0:
+            cnt += 1
+            local_cd = mid - arr[i]
 
-    while i < n:
-        local_time += song_times[i]
-
-        if local_time > time:
-            local_time -= song_times[i]
-            if max_time < local_time:
-                max_time = local_time
-
-            i -= 1
-            dvd += 1
-            local_time = 0
-
-        if i == n-1:
-            if max_time < local_time:
-                max_time = local_time
-            dvd += 1
-        i += 1
-
-    return dvd, max_time
+    return cnt
 
 
+N, M = map(int, input().split())
+arr = list(map(int, input().split()))
 
-n, m = map(int, input().split())
-song_times = list(map(int, input().split()))
+# 한 장의 DVD 당 시간
+s = min(arr)
+e = sum(arr)
 
+min_time = sum(arr)
 
-start = max(song_times)
-max_time = start
-end = sum(song_times)
-min_time = end
+while s <= e:
+    mid = (s + e) // 2
 
-while start <= end:
-    mid = (start + end) // 2
+    # mid 시간짜리 CD 몇 개가 나오지?
+    count = check_count(mid)
 
-    dvd, time = countDVDandTime(mid)
-
-    # dvd 장수가 많다? -> 시간이 작다 -> 시간을 늘려야함.
-
-    if dvd > m:
-        start = mid + 1
-
-    # dvd 장수가 적어도 시간이 가장 적은걸 찾아야함.
-    elif time >= max_time and dvd <= m:
-        end = mid - 1
-        if min_time > time:
-            min_time = time
-
+    # count 가 크다 == DVD 개수가 많다 == DVD 당 시간을 늘린다.
+    # 더 작은 숫자에 넣을 수 있는 것도 답임.
+    # time 은 arr 최댓값 보다 무조건 커야 함 그거보다 작게 나오면 mid 를 늘려서 다시 수행해봐야함.
+    if mid >= max(arr) and count <= M:
+        min_time = mid
+        e = mid - 1
+            
+    else:
+        s = mid + 1
 
 print(min_time)
-
