fc391f92943a45ca6e422b401b58af45654c6073
ddosang <40856360+ddosang@users.noreply.github.com>
2024-05-19 16:15:18 +0900

    240519 복습

--- sec5/5_07.py
+++ sec5/5_07.py
@@ -17,3 +17,26 @@ for index, subject in enumerate(subject_candidates):
         print("#" + str(index + 1) + " YES")
     else:
         print("#" + str(index + 1) + " NO")
+
+
+# 240519 복습
+import copy
+import sys
+input = sys.stdin.readline
+
+order = list(input())
+N = int(input())
+arr = list(input() for _ in range(N))
+
+order.pop()
+for i, a in enumerate(arr):
+    queue = copy.deepcopy(order)
+    for c in a:
+        if c in queue and c == queue[0]:
+            queue.pop(0)
+
+            if len(queue) == 0:
+                print("#" + str(i + 1) + " YES", )
+                break
+    else:
+        print("#" + str(i + 1) + " NO", )
