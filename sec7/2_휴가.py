3ae752ab3444aafbabde1bdf0401a0911a5b96b6
ddosang <40856360+ddosang@users.noreply.github.com>
2024-07-09 17:27:56 +0900

    240709 강의 수강

--- sec7/2_휴가.py
+++ sec7/2_휴가.py
@@ -25,3 +25,25 @@ res = [0] * N
 DFS(0, 0, 0)
 print(max_price)
 
+
+# 240709 강의 코드
+
+N = int(input())
+councils = [list(map(int, input().split())) for _ in range(N)]
+max_cost = 0
+
+def DFS(level, cost):
+    global max_cost
+    if level == N:
+        max_cost = max(cost, max_cost)
+        return
+    
+    # 날짜를 level 로 사용. 이번걸 선택하고
+    if level + councils[level][0] < N:
+        DFS(level + councils[level][0], cost + councils[level][1])
+    # 이번걸 선택하지 않고.
+    DFS(level + 1, cost)
+
+
+DFS(0, 0)
+print(max_cost)
