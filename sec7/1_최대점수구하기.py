65c6b3f2d61eaabdfef5067b46b4bfae6dd9a62e
ddosang <40856360+ddosang@users.noreply.github.com>
2024-07-09 17:05:48 +0900

    Update 1_최대점수구하기.py

--- sec7/1_최대점수구하기.py
+++ sec7/1_최대점수구하기.py
@@ -1,4 +1,9 @@
 # 240701 풀이
+# 첫번째 가지 : 1번 풀고           / 2번 풀고      / 3번 풀고 / 4번 풀고 / 5번 풀고
+# 두번쨰 가지 : 2     3    4   5 /  3     4  5 /  4 5   /  5
+# 세번쨰 가지 : 345 / 45 / 5     // 4 5 / 5   //  5 
+# 1번을 아예 안 푸는게 경우의 수에 없네..?
+
 def DFS(level, total_score, total_time, start):
     global max_score
     if total_time > M:
@@ -28,3 +33,27 @@ visited = [0] * N
 
 DFS(0, 0, 0, 0)
 print(max_score)
+
+
+# 240709 강의 코드
+# n 번 문제를 풀고 안풀고
+N, M = map(int, input().split())
+quizes = [list(map(int, input().split())) for _ in range(N)]
+max_score = 0
+
+def DFS(level, score, time):
+    global max_score
+    if time > M:
+        return
+    
+    if level == N:
+        max_score = max(score, max_score)
+        return
+    
+    DFS(level + 1, score + quizes[level][0], time + quizes[level][1])
+    DFS(level + 1, score, time)
+
+
+DFS(0, 0, 0)
+print(max_score)
+    
