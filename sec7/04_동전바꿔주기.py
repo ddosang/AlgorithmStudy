6b24addf195e7b91263f280ef0cb31bcaa1790fd
ddosang <40856360+ddosang@users.noreply.github.com>
2024-07-10 14:01:05 +0900

    240710 시간복잡도 개선

--- sec7/04_동전바꿔주기.py
+++ sec7/04_동전바꿔주기.py
@@ -2,10 +2,19 @@
 T = int(input())
 K = int(input())
 coins = [list(map(int, input().split())) for _ in range(K)] # 금액 / 개수
+coins.sort(key=lambda x:-x[0])
 cnt = 0
 
 def DFS(level, cost):
     global cnt
+    
+    remain = sum([x[0] * x[1] for x in coins[level:]])
+    
+    if cost + remain < T:
+        return
+
+    if cost > T:
+        return
     
     if cost == T:
         cnt += 1
@@ -20,8 +29,6 @@ def DFS(level, cost):
             break
 
         DFS(level + 1, new_cost)
-        
-
 
 DFS(0, 0)
 print(cnt)
