7baa91bc2a36be3823bbad92ff0812112788b20d
ddosang <40856360+ddosang@users.noreply.github.com>
2024-07-12 23:11:54 +0900

    240712 강의 수강

--- sec7/08_사과나무.py
+++ sec7/08_사과나무.py
@@ -21,3 +21,21 @@ while q:
             res += arr[x+dx][y+dy]
 
 print(res)
+
+
+# 나는 queue 에 level 을 같이 넣었는데, 강의 방법은 level 을 밖으로 빼고, 
+# level 한번마다 큐에 다 넣는걸 이용해서 size 변수 이용 
+level = 0
+while q:
+    if level == N // 2:
+        break
+        
+    size = len(q)
+    for i in range(size):
+        x, y = q.popleft()
+        for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+            if 0 <= x + dx < N and 0 <= y + dy < N and (not visited[x+dx][y+dy]):
+                q.append((x + dx, y + dy))
+                visited[x+dx][y+dy] = True
+                res += arr[x+dx][y+dy]
+    level += 1
