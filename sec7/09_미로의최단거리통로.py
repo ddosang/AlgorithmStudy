5d5dc490cfbc94fff6a011f06ca8b8b2acb26162
ddosang <40856360+ddosang@users.noreply.github.com>
2024-07-14 20:41:22 +0900

    240714 풀이 및 강의 수강

--- sec7/09_미로의최단거리통로.py
+++ sec7/09_미로의최단거리통로.py
@@ -19,3 +19,26 @@ while q:
                 
 print(-1)
 
+
+# 240714 강의 방법
+# 굳이 visited 배열을 따로 둘 필요가 없는 문제였다.
+from collections import deque
+
+arr = [list(map(int, input().split())) for _ in range(7)]
+
+q = deque()
+q.append((0, 0, 0))
+
+while q:
+    x, y, dist = q.popleft()
+
+    for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+        if 0 <= x + dx < 7 and 0 <= y + dy < 7 and arr[x + dx][y + dy] == 0:
+            q.append((x + dx, y + dy, dist + 1))
+            arr[x + dx][y + dy] = 1
+            if x + dx == 6 and y + dy == 6:
+                print(dist + 1)
+                exit(0)
+
+print(-1)
+
